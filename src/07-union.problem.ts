abefc1f8f250f5142f27a8feb943833d27070577
r0binsahin <robin.sahin88@gmail.com>
2024-07-04 11:40:53 +0200

    ex 07 done

--- src/07-union.problem.ts
+++ src/07-union.problem.ts
@@ -1,11 +1,13 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { z } from 'zod';
 
 const Form = z.object({
   repoName: z.string(),
-  privacyLevel: z.string(),
+  privacyLevel: z.union([z.literal('private'), z.literal('public')]),
+  //or using enum
+  //privacyLevel: z.enum(["private", "public"]),
   //              ^ 🕵️‍♂️
 });
 
@@ -17,27 +19,27 @@ export const validateFormInput = (values: unknown) => {
 
 // TESTS
 
-it("Should fail if an invalid privacyLevel passed", async () => {
+it('Should fail if an invalid privacyLevel passed', async () => {
   expect(() =>
     validateFormInput({
-      repoName: "mattpocock",
-      privacyLevel: "something-not-allowed",
-    }),
+      repoName: 'mattpocock',
+      privacyLevel: 'something-not-allowed',
+    })
   ).toThrowError();
 });
 
-it("Should permit valid privacy levels", async () => {
+it('Should permit valid privacy levels', async () => {
   expect(
     validateFormInput({
-      repoName: "mattpocock",
-      privacyLevel: "private",
-    }).privacyLevel,
-  ).toEqual("private");
+      repoName: 'mattpocock',
+      privacyLevel: 'private',
+    }).privacyLevel
+  ).toEqual('private');
 
   expect(
     validateFormInput({
-      repoName: "mattpocock",
-      privacyLevel: "public",
-    }).privacyLevel,
-  ).toEqual("public");
+      repoName: 'mattpocock',
+      privacyLevel: 'public',
+    }).privacyLevel
+  ).toEqual('public');
 });
